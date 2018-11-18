a910e334f0b4fc11b613cb89d51bcf660e9b1ebf
cleptasilus <marcenge@gmail.com>
2018-11-18 18:08:33 +0100

    Update PR_lab1.m
    Last 2 subjects

--- lab1/PR_lab1.m
+++ lab1/PR_lab1.m
@@ -1,11 +1,13 @@
 # Pattern Recognition - Laboratory
 # Petersen, Engelmann, Steeg
 # Exercise L-1.1
-
+clc
+clear all
+close all
 # a: init
-N = 100
-A = fix(rand(N,N) * 10)
-B = round(randn(N,N) * 10)
+N = 100;
+A = fix(rand(N,N) * 10);
+B = round(randn(N,N) * 10);
 
 # b: min/max of each matrix
 amin = min(min(A))
@@ -39,7 +41,6 @@ ylabel("counts of A");
 hold on
 line([1:length(a)],ones(1,length(a))*mean(a));
 hold off
-ishold
 
 # d.2: plot frequency distribution (b)
 figure(2);
@@ -55,7 +56,7 @@ for i = 0:length(a) - 1
  tmp = a(i + 1) / (N*N);
  c = [c tmp];
 endfor
-accPropA = sum(c)
+accPropA = sum(c);
 
 figure(3);
 title("probability distribution of A");
@@ -69,16 +70,34 @@ for i = 0:length(b) - 1
  tmp = b(i+1) / (N*N);
  d = [d tmp];
 endfor
-accPropB = sum(d)
+accPropB = sum(d);
 figure(4);
 title("probability distribution of B");
 bar(d);
 xlabel("B");
 ylabel("rel. frequency of B");
-
+hold on
 # g
 function returnValue = f(x)
-  returnValue = 1 / sqrt(2 * pi * pow2(10))
-  exponent = -( pow2((x)))/(2 * pow2(10))
-  returnValue = returnValue * exp(exponent)
+  returnValue = [];
+  for i=x
+  value = 1 / sqrt(2 * pi * 10^2);
+  exponent = -(i^2)/(2 * 10^2);
+  returnValue = [returnValue (value* e^exponent)];
+  end
 end
+xValues = [bmin:0.1:bmax];
+line(xValues+(-bmin),f(xValues),"color","r");
+
+hold off
+
+%f
+Bflat = B(:);
+meanOfB=mean(Bflat);
+OneStandardDeviation=meanOfB+10;
+TwoStandardDeviation=meanOfB+20;
+ThreeStandardDeviation=meanOfB+30;
+length(Bflat(Bflat>meanOfB-OneStandardDeviation & Bflat<meanOfB+OneStandardDeviation))/(N*N)
+length(Bflat(Bflat>meanOfB-TwoStandardDeviation & Bflat<meanOfB+TwoStandardDeviation))/(N*N)
+length(Bflat(Bflat>meanOfB-ThreeStandardDeviation & Bflat<meanOfB+ThreeStandardDeviation))/(N*N)
+
