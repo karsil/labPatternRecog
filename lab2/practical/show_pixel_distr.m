adac474b5a8359b8b9910118fbb11ff1eb356af6
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2018-11-30 17:24:24 +0100

    cleanup

--- lab2/practical/show_pixel_distr.m
+++ lab2/practical/show_pixel_distr.m
@@ -6,22 +6,15 @@ clc
 clear all
 close all
 
-%task c:
-
-%load("digit3.mat")
-%hist(D(:,297),length(unique(D)))
-%figure(1)
-%tempImage = 0*D;
-%tempImage(:, 297) = tempImage(:,297)+255;
-%tempImage = reshape(tempImage(1,:),[28,28]);
-%figure(2),imshow(tempImage',[]);
-
-%task d:
+%task c/d:
 function displayPixel(pixelPositions)
   load("materials/digit3.mat")
   figureNumber=1;
   for i=pixelPositions
+    # task c
     figure(figureNumber+1),hist(D(:,i),length(unique(D)))
+    
+    # task d
     tempImage = 0*D;
     tempImage(:, i) = tempImage(:,i)+255;
     tempImage = reshape(tempImage(1,:),[28,28]);
@@ -30,7 +23,7 @@ function displayPixel(pixelPositions)
   end
 end
 
-%hier muss noch einer "interessante" stellen raussuchen ^^
+# show *interesting* pixel positions
 displayPixel([297,126,151,178,348,404,467,523,577,603,658])
 
 
