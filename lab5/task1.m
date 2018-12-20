77b261051bd341cb51a30f2daeabc78db12859e1
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2018-12-20 12:01:43 +0100

    init lab5

---
+++ lab5/task1.m
@@ -0,0 +1,48 @@
+# Pattern Recognition - Laboratory
+# Petersen, Engelmann, Steeg
+# Exercise L-5.1
+
+
+load("materials/patterns.txt"); # patterns
+load("materials/targets.txt"); # targets
+load("materials/twoClasses.mat");
+
+# Part 0, Introduction
+
+# Show the first 5 observations of class omega0:
+# patterns(:,1:5)
+# should result in
+# ans =
+#    3.6642    4.9162    3.9818    5.3453    2.5478
+#   20.0939   15.3323   16.6113   15.4816   18.4773
+
+# Show the first 5 class labels of class ω1 >> targets(2001:2005)
+# ans =
+#   11111
+
+
+
+
+### Part a
+# organize the data in a cell array with appropriate labeling of the
+# - classes as ‘Class0’ and ‘Class1’
+# - variables as ‘Sensor1’ and ‘Sensor2’
+# - observations as ‘Obs_1’ to ‘Obs_2000’
+
+# Example:
+# Show the cell array content of the first two observations of the first sensor for class one. >> cloud1(1:3, 2, 1)
+# ans = Sensor1
+# ans =  3.6642
+# ans =  4.9162
+
+
+
+### Part b
+
+
+
+### Part c
+
+
+
+
