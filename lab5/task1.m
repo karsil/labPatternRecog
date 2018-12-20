77f00ed2803797cf042379df138585d3232fcc69
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2018-12-20 13:49:21 +0100

    word in progress, task a

--- lab5/task1.m
+++ lab5/task1.m
@@ -32,12 +32,24 @@ targets(2001:2005)
 # - observations as ‘Obs_1’ to ‘Obs_2000’
 
 # Example:
-# Show the cell array content of the first two observations of the first sensor for class one. >> cloud1(1:3, 2, 1)
+# Show the cell array content of the first two observations of the first sensor for class one.
+# cloud1(1:3, 2, 1)
 # ans = Sensor1
 # ans =  3.6642
 # ans =  4.9162
 
+# Solution
 
+offset = 2000
+offsetDev = 3
+cloud1 = zeros(2,2, offsetDev)
+
+# for i = 1:length(targets)
+for i = 1:offsetDev # 5 for development
+  new = [patterns(:,i) patterns(:,i+offset)]
+  cloud1(:,:,i) = new # WIP not correct order in matrix
+  size(cloud1)
+end
 
 ### Part b
 
