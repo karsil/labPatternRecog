a50afb7793ff516803b0e5e32fb5e70f61cff639
lapape <lasse.b.petersen@student.fh-kiel.de>
2019-01-09 22:29:41 +0100

    Some more minor changes

--- lab6/Lab6.m
+++ lab6/Lab6.m
@@ -6,9 +6,9 @@ nonFaceFeats = [];
 numPix = 0;
 for i = 1:size(train_images,1)
   I = train_images(i,:);
-  I = reshape(I, [112,92]); % get next image from image matrix
+  I = reshape(I, [112,92]);
   P = train_patterns(i,:);
-  P = reshape(P, [112,92]); % get next label matrix
+  P = reshape(P, [112,92]);
   B = im2col(padarray(I, [1, 1], 0, 'both'), [3, 3], 'sliding');
   faceFeatsNew = B(:,logical(P(:)));
   nonFaceFeatsNew = B(:,~logical(P(:)));
@@ -27,9 +27,11 @@ for i = 1:size(test_images,1)
   I = test_images(i,:);
   I = reshape(I, [112,92]);
   B = im2col(padarray(I, [1, 1], 0, 'both'), [3, 3], 'sliding');
+  
   dist_1 = sum((double(B) - repmat(meanFaces',[1 size(B,2)])).^2);
   dist_2 = sum((double(B) - repmat(meanNonFaces',[1 size(B,2)])).^2);
   result = dist_1 < dist_2;
+  
   classified = reshape(result, 112, 92);
   figure(1), imshow(classified);
   pause(0.1);
@@ -49,7 +51,6 @@ for i = 1:size(test_images,1)
   
   p_x_1 = mvnpdf(double(B'), meanFaces, covFaces);
   p_x_2 = mvnpdf(double(B'), meanNonFaces, covNonFaces);
-
   p_x_1_p_1= p_x_1 *p1;
   p_x_2_p_2= p_x_2 *p2;
   result = p_x_1_p_1 > p_x_2_p_2;
