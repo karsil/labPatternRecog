98ac5e82028c3a7e729013a32450865fd7a5e850
cleptasilus <marcenge@gmail.com>
2019-02-07 20:42:50 +0100

    Update lab8.m

--- lab8/lab8.m
+++ lab8/lab8.m
@@ -1,9 +1,10 @@
 
 c=10;
 errors=randn(1,100000);
-sigmaE=10
+sigmaE=100
 logCP=log(1/(sqrt(2*pi)*1)*exp(-1/2*((c/1).^2)))
 result=((c+1/(sqrt(2*pi)*sigmaE).*exp(-1/2.*(((errors)./sigmaE).^2))));
 figure(1)
 hist(result)
 disp(max(result))
+disp((logCP+sum(result))/100000)
